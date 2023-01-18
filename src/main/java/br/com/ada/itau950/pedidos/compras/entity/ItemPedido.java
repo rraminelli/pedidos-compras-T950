48075bb4dceafb95416b05bddc45ca9043c74bd3
Rodrigo Raminelli <rraminelli@gmail.com>
2023-01-18 22:12:36 +0100

    aula 5

--- src/main/java/br/com/ada/itau950/pedidos/compras/entity/ItemPedido.java
+++ src/main/java/br/com/ada/itau950/pedidos/compras/entity/ItemPedido.java
@@ -1,5 +1,6 @@
 package br.com.ada.itau950.pedidos.compras.entity;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,13 +8,25 @@ import java.math.BigDecimal;
 
 @Getter
 @Setter
+@Entity
 public class ItemPedido {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id")
     private Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id")
     private Produto produto;
+
     private Integer quantidade;
+
     private BigDecimal preco;
+
     private BigDecimal desconto;
 
 }
--- src/main/java/br/com/ada/itau950/pedidos/compras/entity/Pedido.java
+++ src/main/java/br/com/ada/itau950/pedidos/compras/entity/Pedido.java
@@ -1,6 +1,7 @@
 package br.com.ada.itau950.pedidos.compras.entity;
 
 import br.com.ada.itau950.pedidos.compras.entity.enums.StatusPedidoEnum;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,13 +10,24 @@ import java.util.Set;
 
 @Getter
 @Setter
+@Entity
 public class Pedido {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
     private Usuario usuario;
+
     private LocalDateTime dataPedido;
+
     private String mensagemStatus;
+
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
     private Set<ItemPedido> itens;
+
+    @Enumerated(EnumType.STRING)
     private StatusPedidoEnum status;
 
 }
